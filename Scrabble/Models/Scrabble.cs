9a4a1648afdefef14d0b3a730bfca4e6cbec81ca
Matt Stroud <lixingke06@gmail.com>
2020-05-14 10:06:24 -0700

    update GetLetterScores to return value of A

--- Scrabble/Models/Scrabble.cs
+++ Scrabble/Models/Scrabble.cs
@@ -1,13 +1,23 @@
+using System.Collections.Generic;
 
 namespace ScrabbleScore.Models
 {
   public class Scrabble
   {
     public string Word { get; set; }
+    private static Dictionary<char, int> _letterScores = new Dictionary<char, int>
+    {
+      { 'a', 1 }
+    };
 
     public Scrabble(string word)
     {
       Word = word.ToLower();
+    }
+
+    public static int GetLetterScore(char letter)
+    {
+      return _letterScores[letter];
     }
   }
 }
