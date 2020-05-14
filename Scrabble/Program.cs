979dfff4b3ff0b81f6def5ef5433f2f8ed8d3289
Matt Stroud <lixingke06@gmail.com>
2020-05-14 10:47:47 -0700

    update console UI to accept user input and display scores

--- Scrabble/Program.cs
+++ Scrabble/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using ScrabbleScore.Models;
 
 namespace ScrabbleScore
 {
@@ -6,7 +7,22 @@ namespace ScrabbleScore
   {
     public static void Main()
     {
+      string userInput = "";
 
+      while (userInput != "...")
+      {
+        Console.WriteLine("Enter a word to have it scored, or enter \"...\" to quit");
+        userInput = Console.ReadLine();
+        if (userInput != "...")
+        {
+          Scrabble word = new Scrabble(userInput);
+          Console.WriteLine("The score for " + userInput + " is " + word.GetWordScore() + ".");
+        }
+        else
+        {
+          Console.WriteLine("Goodbye.");
+        }
+      }
     }
   }
 }
