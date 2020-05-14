72f7ff54bc92a51489a1805434325dc49700a855
Matt Stroud <lixingke06@gmail.com>
2020-05-14 09:52:54 -0700

    add code to constructor to store word

--- Scrabble.Tests/ModelTests/ScrabbleTests.cs
+++ Scrabble.Tests/ModelTests/ScrabbleTests.cs
@@ -6,6 +6,15 @@ namespace ScrabbleScore.Tests
   [TestClass]
   public class ScrabbleTests
   {
+    [TestMethod]
+    public void Scrabble_TakesWordAsInput_Word()
+    {
+      string newWord = "Hello";
+      Scrabble scrabble = new Scrabble(newWord);
 
+      string result = scrabble.Word;
+
+      Assert.AreEqual(result, newWord);
+    }
   }
 }
--- Scrabble/Models/Scrabble.cs
+++ Scrabble/Models/Scrabble.cs
@@ -3,6 +3,11 @@ namespace ScrabbleScore.Models
 {
   public class Scrabble
   {
+    public string Word { get; set; }
 
+    public Scrabble(string word)
+    {
+      Word = word;
+    }
   }
 }
